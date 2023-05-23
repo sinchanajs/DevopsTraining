96b3d2ec537057404e69f046be89f511ba94bec3
EXCELINDIA\sinchana.js <sinchana.js@excelindia.com>
2023-05-23 10:44:16 +0530

    first commit

---
+++ BatchDemo.bat
@@ -0,0 +1,5 @@
+@echo off
+set sourceFolder=C:\Users\sinchana.js\Pictures\Screenshots\Screenshot (156).png
+set destinationFolder=D:\AwsDemo\Tasks
+
+xcopy /E /I /Y "%sourceFolder%\*" "%destinationFolder%\"
